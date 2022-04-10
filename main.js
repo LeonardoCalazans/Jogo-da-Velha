a6bf4ebb7845968aadf7b907a55dc825afa11eff
WendelNogueira23 <wendel.felipe89@gmail.com>
2022-04-10 16:33:19 -0300

    finalizado lógica do jogo

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+// import { 
+//     buttonPlayNow,
+//     onePlayer as player1,
+//     twoPlayer as player2
+//  } from "./modules/buttonPlay.js";
 import buttonPlayNow from "./modules/buttonPlay.js";
 /*
 PONTOS DA AV1:    
@@ -12,18 +17,112 @@ PONTOS DA AV1:
     AO FINAL DO MELHOR DE TRES O VENCEDOR DEVERÁ TER O NOME APRESENTADO NO LUGAR DO JOGO DA VELHA
     DEVERÁ SER ATUALIZADO O HISTÓRICO DAS PARTIDAS 
 */
-const onePlayer = document.getElementById('onePlayer');
-const twoPlayer = document.getElementById('twoPlayer');
+const linesPlayed = document.querySelectorAll(".line-played");
+const board = document.getElementById("board");
+const winningMessageTextElement = document.querySelector(".winning-message-text");
+const winningMessage = document.querySelector(".winning-message");
+const restartButton = document.querySelector("#buttonPlayAgain");
+const revangeButton = document.querySelector("#buttonRevanche");
+const recordsButton = document.querySelector("#buttonRecords");
 
-buttonPlayNow();
-function comecar() {
+let isCircleTurn;
 
-    // EXEMPLO DE MANIPULACAO E RECUPERACAO DE DADOS DA DOM
-    // let conteudo = document.getElementById("botao-iniciar");
-    // console.log(conteudo.innerHTML)
-    // conteudo.innerHTML = "Valendo!!!"
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+const checkForWin = (currentPlayer) => {
+    return winningCombinations.some(combination => {
+        return combination.every(index => {
+            return linesPlayed[index].classList.contains(currentPlayer);
+        });
+    });
+};
+
+const placeMark = (line, classToAdd) => {
+    line.classList.add(classToAdd);
+}
+
+const setBoardHoverClass = () => {
+    board.classList.remove("circle");
+    board.classList.remove("x");
+
+    if(isCircleTurn) {
+        board.classList.add("circle");
+    } else {
+        board.classList.add("x");
+    }
+}
+
+const swapTurn = () => {
+    isCircleTurn = !isCircleTurn;
+    setBoardHoverClass();
+}
+
+const endGame = (isDraw) => {
+    if(isDraw) {
+        winningMessageTextElement.innerText = "Deu velha!";
+    } else {
+        winningMessageTextElement.innerText = `O jogador ${isCircleTurn ? "O" : "X"} venceu!`;
+    }
+
+    winningMessage.classList.remove("hidden");
+}
+
+const checkEndGame = (currentPlayer) => {
+    const isWin = checkForWin(currentPlayer);
+    if(isWin) {
+        endGame(false);
+    } else {
+        const isDraw = checkForDraw();
+        if(isDraw) {
+            endGame(true);
+        }
+    }
 
 }
+
+const checkForDraw = () => {
+    return [...linesPlayed].every(line => {
+        return line.classList.contains("circle") || line.classList.contains("x");
+    });
+}
+
+const handleClick = (event) => {
+    const line = event.target;
+    const classToAdd = isCircleTurn ? "circle" : "x";
+
+    placeMark(line, classToAdd);
+
+    checkEndGame(classToAdd);
+
+    swapTurn();
+}
+
+function startGame() {
+    // console.log(player1, player2);
+
+    for (const line of linesPlayed) {
+        line.classList.remove("circle");
+        line.classList.remove("x");
+        line.removeEventListener("click", handleClick);
+        line.addEventListener('click', handleClick, { once: true });
+    }
+
+    setBoardHoverClass();
+    winningMessage.classList.add("hidden");
+}
+
+buttonPlayNow();
+startGame();
+restartButton.addEventListener('click', startGame);
 
 
 
