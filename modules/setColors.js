bf2741db9f2efedecde5ebae54489067bba5fa52
Leonardo Pinto Silva Calazans <leonardopintosilva@hotmail.com>
2022-04-19 20:06:41 -0300

    leonardo/feat(colors): adicionando mais opções de cores ao layout

---
+++ modules/setColors.js
@@ -0,0 +1,34 @@
+const pink = document.getElementById("pink");
+const blue = document.getElementById("blue");
+const green = document.getElementById("green");
+const red = document.getElementById("red");
+const dark = document.getElementById("dark");
+
+const body = document.getElementById("body");
+
+pink.addEventListener('click', setColors)
+blue.addEventListener('click', setColors)
+green.addEventListener('click', setColors)
+red.addEventListener('click', setColors)
+dark.addEventListener('click', setColors)
+
+
+function setLocalStorage(color) {
+    return localStorage.setItem("color", JSON.stringify(color));
+}
+
+if (localStorage.getItem('color')) {
+    console.log('entrou')
+    let getColor = JSON.parse(localStorage.getItem('color'))
+    console.log(getColor)
+    setLocalStorage(getColor);
+    body.classList.add(getColor);
+}
+
+export default function setColors(event) {
+    let bodyClassRemove = body.classList[0];
+    let bodyClassAdd = event.path[0].id;
+    body.classList.remove(bodyClassRemove);
+    body.classList.add(bodyClassAdd);
+    setLocalStorage(bodyClassAdd);
+}
